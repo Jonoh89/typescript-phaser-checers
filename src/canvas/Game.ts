10b4d99d0785e1e7e90047873ca255c79e8bb275
Jonathan Holmlund <jonoh89@gmail.com>
2017-09-01 21:39:36 +0200

    add prettier ts and tsx

--- src/canvas/Game.ts
+++ src/canvas/Game.ts
@@ -1,18 +1,19 @@
 import * as Phaser from 'phaser-ce';
 
 class Game {
-    game: Phaser.Game;
+  game: Phaser.Game;
 
-    constructor(canvas: HTMLElement) {
-        this.game = new Phaser.Game(800, 600, Phaser.AUTO, canvas, { create: this.create });
-    }
+  constructor(canvas: HTMLElement) {
+    this.game = new Phaser.Game(800, 600, Phaser.AUTO, canvas, {
+      create: this.create
+    });
+  }
 
-    create() {
-        const text = 'Hello World!';
-        const style = { font: '65px Arial', fill: '#ff0000', align: 'center' };
-        this.game.add.text(0, 0, text, style);
-    }
-
+  create() {
+    const text = 'Hello World!';
+    const style = { font: '65px Arial', fill: '#ff0000', align: 'center' };
+    this.game.add.text(0, 0, text, style);
+  }
 }
 
 export default Game;
--- src/components/GameCanvas.tsx
+++ src/components/GameCanvas.tsx
@@ -2,22 +2,19 @@ import * as React from 'react';
 import Game from '../canvas/Game';
 
 class GameCanvas extends React.Component {
-    c: HTMLDivElement | null;
+  c: HTMLDivElement | null;
 
-    componentDidMount() {
-        if (this.c) {
-            new Game(this.c);
-        } else {
-            throw new Error('No element available');
-        }
+  componentDidMount() {
+    if (this.c) {
+      new Game(this.c);
+    } else {
+      throw new Error('No element available');
     }
+  }
 
-    render() {
-        return (
-            <div ref={(c => this.c = c)} />
-        );
-    }
-
+  render() {
+    return <div ref={c => (this.c = c)} />;
+  }
 }
 
 export default GameCanvas;
